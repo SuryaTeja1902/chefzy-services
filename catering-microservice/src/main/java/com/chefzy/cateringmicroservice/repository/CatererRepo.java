b03cf0bc807741c1d51117c10117e3232e708881
Abhishek <abhipodila1@gmail.com>
2025-02-23 22:07:09 -0500

    - Added Catering Microservice - Exception Handling in Progress

---
+++ catering-microservice/src/main/java/com/chefzy/cateringmicroservice/repository/CatererRepo.java
@@ -0,0 +1,17 @@
+package com.chefzy.cateringmicroservice.repository;
+
+import com.chefzy.cateringmicroservice.entity.Caterer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CatererRepo extends JpaRepository<Caterer,Long> {
+
+    @Query("select c from Caterer c WHERE c.eventType =?1")
+    List<Caterer> findAllCaterersByEvent(String eventType);
+
+
+}
