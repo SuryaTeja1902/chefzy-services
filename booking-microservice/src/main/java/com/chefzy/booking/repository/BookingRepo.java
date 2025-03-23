9003347ee8f9a398a8c55e9b9c1133455a63d7af
Abhishek <abhipodila1@gmail.com>
2025-03-23 17:20:56 -0400

    Created Booking Microservice

---
+++ booking-microservice/src/main/java/com/chefzy/booking/repository/BookingRepo.java
@@ -0,0 +1,9 @@
+package com.chefzy.booking.repository;
+
+import com.chefzy.booking.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepo extends JpaRepository<Booking,Long> {
+}
