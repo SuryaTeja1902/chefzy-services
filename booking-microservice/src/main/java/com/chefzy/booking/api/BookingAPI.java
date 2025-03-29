763c6dbf3dc023f0f2b935824bea158b0b43fa08
Abhishek <abhipodila1@gmail.com>
2025-03-29 19:39:16 -0400

    Added following APIs: - findBookingByCatererId - findBookingByChefId - GetAllBookingsByConsumerId - GetAllBookingsByChefId - GetAllBookingsByCatererId

--- booking-microservice/src/main/java/com/chefzy/booking/api/BookingAPI.java
+++ booking-microservice/src/main/java/com/chefzy/booking/api/BookingAPI.java
@@ -2,20 +2,39 @@ package com.chefzy.booking.api;
 
 import com.chefzy.booking.dto.BookingDTO;
 import com.chefzy.booking.dto.BookingResponseDTO;
+import jakarta.xml.bind.ValidationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface BookingAPI {
 
     @PostMapping("/chef/{chef_id}")
-    ResponseEntity<BookingResponseDTO> addChefBooking(@PathVariable("chef_id") long chef_id, @RequestBody @Validated BookingDTO bookingDTO);
+    ResponseEntity<BookingResponseDTO> addChefBooking(@PathVariable("chef_id") Long chef_id, @RequestBody @Validated BookingDTO bookingDTO);
 
     @PostMapping("/caterer/{caterer_id}")
-    ResponseEntity<BookingResponseDTO> addCatererBooking(@PathVariable("caterer_id") long caterer_id, @RequestBody @Validated BookingDTO bookingDTO);
+    ResponseEntity<BookingResponseDTO> addCatererBooking(@PathVariable("caterer_id") Long caterer_id, @RequestBody @Validated BookingDTO bookingDTO);
 
+    @GetMapping("/chef/{chef_id}")
+    ResponseEntity<?> getChefBookingById(@PathVariable("chef_id") Long chef_id) throws ValidationException;
+
+    @GetMapping("/caterer/{caterer_id}")
+    ResponseEntity<?> getCatererBookingById(@PathVariable("caterer_id") Long caterer_id) throws ValidationException;
+
+    @GetMapping("/consumer/{consumer_id}/all")
+    ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsbyConsumerId(@PathVariable("consumer_id") Long consumer_id) throws ValidationException;
+
+    @GetMapping("/chef/{chef_id}/all")
+    ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsbyChefId(@PathVariable("chef_id") Long chef_id) throws ValidationException;
+
+    @GetMapping("/caterer/{caterer_id}/all")
+    ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsbyCatererId(@PathVariable("caterer_id") Long caterer_id) throws ValidationException;
 
 
 }
