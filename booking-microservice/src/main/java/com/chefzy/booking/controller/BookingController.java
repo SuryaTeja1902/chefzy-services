eb25378485d1113408a9d730a0a1a1da4fd04c23
Chaitanya Sai <dcsai54@gmail.com>
2025-07-05 01:21:03 +0530

    Refactor Booking service and controller for improved readability and consistency

--- booking-microservice/src/main/java/com/chefzy/booking/controller/BookingController.java
+++ booking-microservice/src/main/java/com/chefzy/booking/controller/BookingController.java
@@ -8,33 +8,33 @@ import com.chefzy.booking.mapper.BookingMapper;
 import com.chefzy.booking.service.BookingService;
 import jakarta.xml.bind.ValidationException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/bookings")
 @Slf4j
 public class BookingController implements BookingAPI {
 
-    @Autowired
-    private BookingService bookingService;
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
 
     @Override
     public ResponseEntity<BookingResponseDTO> addChefBooking(Long chef_id, BookingDTO bookingDTO) {
 
         log.info("Received request to create a new booking with chef id");
-        try{
+        try {
             Booking booking = bookingService.addChefBooking(chef_id, bookingDTO);
             log.info("Chef Booking created with  ID - {}", chef_id);
             return ResponseEntity.status(HttpStatus.CREATED).body(BookingMapper.mapBookingToBookingResponseDTO(booking));
-        }
-        catch (ValidationException exception){
+        } catch (ValidationException exception) {
             log.error("Validation error: {}", exception.getMessage());
             return ResponseEntity.badRequest().body(null);
         }
@@ -44,12 +44,11 @@ public class BookingController implements BookingAPI {
     public ResponseEntity<BookingResponseDTO> addCatererBooking(Long caterer_id, BookingDTO bookingDTO) {
 
         log.info("Received request to create a new booking with caterer id");
-        try{
+        try {
             Booking booking = bookingService.addCatererBooking(caterer_id, bookingDTO);
             log.info("Caterer Booking created with  ID - {}", caterer_id);
             return ResponseEntity.status(HttpStatus.CREATED).body(BookingMapper.mapBookingToBookingResponseDTO(booking));
-        }
-        catch (ValidationException exception){
+        } catch (ValidationException exception) {
             log.error("Validation error: {}", exception.getMessage());
             return ResponseEntity.badRequest().body(null);
         }
@@ -59,67 +58,60 @@ public class BookingController implements BookingAPI {
     @Override
     public ResponseEntity<?> getChefBookingById(Long chef_id) {
         log.info("Received request to get chef booking with ID - {}", chef_id);
-        try{
+        try {
             Booking booking = bookingService.getChefBookingById(chef_id);
             return ResponseEntity.status(HttpStatus.OK).body(BookingMapper.mapBookingToBookingResponseDTO(booking));
-        }
-        catch (ValidationException exception){
+        } catch (ValidationException exception) {
             log.error("Validation error: {}", exception.getMessage());
             return ResponseEntity.notFound().build();
         }
     }
-
 
     @Override
     public ResponseEntity<?> getCatererBookingById(Long caterer_id) {
         log.info("Received request to get caterer booking with ID - {}", caterer_id);
-        try{
+        try {
             Booking booking = bookingService.getCatererBookingById(caterer_id);
             return ResponseEntity.status(HttpStatus.OK).body(BookingMapper.mapBookingToBookingResponseDTO(booking));
-        }
-        catch (ValidationException exception){
+        } catch (ValidationException exception) {
             log.error("Validation error: {}", exception.getMessage());
             return ResponseEntity.notFound().build();
         }
     }
 
     @Override
-    public ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsbyConsumerId(Long consumer_id) throws ValidationException {
+    public ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsByConsumerId(Long consumer_id) {
         log.info("Received request to get all bookings with consumer ID - {}", consumer_id);
-        try{
+        try {
             ArrayList<BookingResponseDTO> allBookings = (ArrayList<BookingResponseDTO>) bookingService.getAllBookingsByConsumerId(consumer_id);
             return ResponseEntity.status(HttpStatus.OK).body(allBookings);
-        }
-        catch (ValidationException exception){
+        } catch (ValidationException exception) {
             log.error("Validation error: {}", exception.getMessage());
             return ResponseEntity.notFound().build();
         }
     }
 
     @Override
-    public ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsbyChefId(Long chef_id) throws ValidationException {
+    public ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsByChefId(Long chef_id) {
         log.info("Received request to get all bookings by chef ID - {}", chef_id);
-        try{
+        try {
             ArrayList<BookingResponseDTO> allBookings = (ArrayList<BookingResponseDTO>) bookingService.getAllBookingsByChefId(chef_id);
             return ResponseEntity.status(HttpStatus.OK).body(allBookings);
-        }
-        catch (ValidationException exception){
+        } catch (ValidationException exception) {
             log.error("Validation error: {}", exception.getMessage());
             return ResponseEntity.notFound().build();
         }
     }
 
     @Override
-    public ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsbyCatererId(Long caterer_id) throws ValidationException {
+    public ResponseEntity<ArrayList<BookingResponseDTO>> getAllBookingsByCatererId(Long caterer_id) {
         log.info("Received request to get all bookings by caterer ID - {}", caterer_id);
-        try{
+        try {
             ArrayList<BookingResponseDTO> allBookings = (ArrayList<BookingResponseDTO>) bookingService.getAllBookingsByCatererId(caterer_id);
             return ResponseEntity.status(HttpStatus.OK).body(allBookings);
-        }
-        catch (ValidationException exception){
+        } catch (ValidationException exception) {
             log.error("Validation error: {}", exception.getMessage());
             return ResponseEntity.notFound().build();
         }
     }
-    }
-
+}
